dfaf8c0ffea2de49d55245070fa3ed90062f21b4
Riley-77 <rpickford2024@elam.co.uk>
2024-10-07 15:39:37 +0100

    Death Screen complete/ Main menu 50%

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject MainMenuCanvas;
+
+    public void ShowMainMenu()
+    {
+        MainMenuCanvas.SetActive(true);
+
+        Time.timeScale = 1f;
+    }
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+}
+
